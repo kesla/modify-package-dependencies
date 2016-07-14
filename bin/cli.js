032fd20fba08a5763b5e3046d2366e4141ce2927
David Bjorklund <david.bjorklund@gmail.com>
2016-07-14 16:02:54 +0200

    add cli

---
+++ bin/cli.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+require('../dist/cli').default(process.argv.slice(2));
+
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 import test from 'tapava';
 import {inject} from './lib';
+import parseArgs from './lib/parse-args';
 
 test('add() default', function * (t) {
   const getPackage = function (arg) {
@@ -233,4 +234,30 @@ test('remove() when package is in devDependencies', t => {
     devDependencies: {}
   };
   t.deepEqual(actual, expected);
+});
+
+test('parseArgs()', t => {
+  const expected1 = {
+    add: ['add1', 'add2'],
+    addDev: ['add-dev1', 'add-dev2'],
+    remove: ['remove1', 'remove2']
+  };
+  const actual1 = parseArgs('--add add1 --add-dev add-dev1 --remove remove1 -p add2 -d add-dev2 -r remove2'.split(' '));
+  t.match(actual1, expected1);
+
+  const expected2 = {
+    add: ['add'],
+    addDev: ['add-dev'],
+    remove: ['remove']
+  };
+  const actual2 = parseArgs('--add add --add-dev add-dev --remove remove'.split(' '));
+  t.match(actual2, expected2);
+
+  const expected3 = {
+    add: [],
+    addDev: [],
+    remove: []
+  };
+  const actual3 = parseArgs([]);
+  t.match(actual3, expected3);
 });
