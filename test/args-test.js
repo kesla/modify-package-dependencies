3831e476117d8872484e8b9d7657ea3366603bd9
David Bjorklund <david.bjorklund@gmail.com>
2016-07-14 18:20:01 +0200

    add cli

--- test/args-test.js
+++ test/args-test.js
@@ -2,11 +2,12 @@ import test from 'tapava';
 import {inject} from '../lib';
 
 test('.args', t => {
+  t.plan(1);
   const _packages = {
-    add1: '1',
-    add2: '2',
-    addDev1: '3',
-    addDev2: '4'
+    'add1': {name: 'add1', version: '1'},
+    'add2': {name: 'add2', version: '2'},
+    'add-dev1': {name: 'add-dev1', version: '3'},
+    'add-dev2': {name: 'add-dev2', version: '4'}
   };
   const getPackage = arg => Promise.resolve(_packages[arg]);
   const packageJson = Object.freeze({
@@ -22,18 +23,19 @@ test('.args', t => {
   const expected = {
     dependencies: {
       keep1: '1',
-      add1: '1',
-      add2: '2'
+      add1: '^1',
+      add2: '^2'
     },
     devDependencies: {
-      keep2: '1',
-      addDev1: '3',
-      addDev2: '4'
+      'keep2': '1',
+      'add-dev1': '^3',
+      'add-dev2': '^4'
     }
   };
   const {args} = inject(getPackage);
+  const inputArgs = '--add add1 --add-dev add-dev1 --remove remove1 -p add2 -d add-dev2 -r remove2';
 
-  args('--add add1 --add-dev add-dev1 --remove remove1 -p add2 -d add-dev2 -r remove2'.split(' '))
+  return args(packageJson, inputArgs.split(' '))
     .then(actual => {
       t.deepEqual(actual, expected);
     });
