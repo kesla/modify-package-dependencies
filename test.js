1a88bdb51b7f48237c53273800d1687dbb8bf5ea
David Bjorklund <david.bjorklund@gmail.com>
2016-07-14 16:54:19 +0200

    remove() take packageNames array

--- test.js
+++ test.js
@@ -208,34 +208,51 @@ test('addDev() when package already is in dependencies', function * (t) {
     });
 });
 
-test('remove() when package is in dependencies', t => {
+test('remove() when packages is in dependencies', t => {
   const getPackage = () => {
     throw new Error('should not be called');
   };
 
   const {remove} = inject(getPackage);
   const packageJson = Object.freeze({dependencies: {packageName: '1.2.3'}});
-  const actual = remove(packageJson, 'packageName');
+  const actual = remove(packageJson, ['packageName']);
   const expected = {
     dependencies: {}
   };
   t.deepEqual(actual, expected);
 });
 
-test('remove() when package is in devDependencies', t => {
+test('remove() when packages is in devDependencies', t => {
   const getPackage = () => {
     throw new Error('should not be called');
   };
 
   const {remove} = inject(getPackage);
   const packageJson = Object.freeze({devDependencies: {packageName: '1.2.3'}});
-  const actual = remove(packageJson, 'packageName');
+  const actual = remove(packageJson, ['packageName']);
   const expected = {
     devDependencies: {}
   };
   t.deepEqual(actual, expected);
 });
 
+test('remove() when packages is in devDependencies & dependencies', t => {
+  const getPackage = () => {
+    throw new Error('should not be called');
+  };
+
+  const {remove} = inject(getPackage);
+  const packageJson = Object.freeze({
+    dependencies: {packageName1: '1.2.3'},
+    devDependencies: {packageName2: '2.3.4'}
+  });
+  const actual = remove(packageJson, ['packageName1', 'packageName2']);
+  const expected = {
+    dependencies: {},
+    devDependencies: {}
+  };
+  t.deepEqual(actual, expected);
+});
 test('parseArgs()', t => {
   const expected1 = {
     add: ['add1', 'add2'],
